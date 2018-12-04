5751c32aa29a961921f9508d2a350adfa7019fc5
= <=>
2018-12-04 18:08:13 +0700

    bắt đầu làm đồ án nè

--- app/src/main/java/com/example/thanh/apphtlshop/MainActivity.java
+++ app/src/main/java/com/example/thanh/apphtlshop/MainActivity.java
@@ -1,32 +1,97 @@
 package com.example.thanh.apphtlshop;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.example.thanh.model.SanPham;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ListView lvSanPham;
-    private ArrayAdapter<SanPham> arrSanPham;
-    private ArrayList<SanPham> listSanPham;
+    ListView lvDanhSachSanPham;
+    ArrayAdapter<SanPham> sanPhamArrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         addControls();
+        addEvents();
     }
 
     private void addControls() {
-        lvSanPham = findViewById(R.id.lvSanPham);
-        listSanPham = new ArrayList<>();
-        arrSanPham = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listSanPham);
-        lvSanPham.setAdapter(arrSanPham);
+        lvDanhSachSanPham = findViewById(R.id.lvSanPham);
+        sanPhamArrayAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1);
+        lvDanhSachSanPham.setAdapter(sanPhamArrayAdapter);
+        DanhSachSanPhamTask danhSachSanPhamTask = new DanhSachSanPhamTask();
+        danhSachSanPhamTask.execute();
+    }
 
+    private void addEvents() {
+
+    }
+
+    class DanhSachSanPhamTask extends AsyncTask<Void, Void, ArrayList<SanPham>> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<SanPham> sanPhams) {
+            super.onPostExecute(sanPhams);
+            sanPhamArrayAdapter.clear();
+            sanPhamArrayAdapter.addAll(sanPhams);
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected ArrayList<SanPham> doInBackground(Void... voids) {
+            ArrayList<SanPham> dsSanPham = new ArrayList<>();
+            try {
+                URL url = new URL("http://www.tripletstore.somee.com/api/sanpham");
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("GET");
+                httpURLConnection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+                InputStreamReader isr = new InputStreamReader(httpURLConnection.getInputStream(), "UTF-8");
+                BufferedReader br = new BufferedReader(isr);
+                StringBuilder builder = new StringBuilder();
+                String line = null;
+                while ((line = br.readLine()) != null) {
+                    builder.append(line);
+                }
+                JSONArray jsonArray = new JSONArray(builder.toString());
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    int masp = jsonObject.getInt("masp");
+                    String tensp = jsonObject.getString("tensp");
+                    SanPham sp = new SanPham();
+                    sp.setTenSp(tensp);
+                    sp.setMaSp(masp);
+                    dsSanPham.add(sp);
+                }
+                br.close();
+                isr.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return dsSanPham;
+        }
     }
 }
--- app/src/main/java/com/example/thanh/model/SanPham.java
+++ app/src/main/java/com/example/thanh/model/SanPham.java
@@ -3,7 +3,7 @@ package com.example.thanh.model;
 public class SanPham {
     private int maSp;
     private String tenSp;
-    private int maNcc;
+ /*   private int maNcc;
     private int maLoai;
     private int soLuongTon;
     private String anhBia;
@@ -12,40 +12,14 @@ public class SanPham {
     private String moTa;
     private String ngayCapNhat;
     private int slBanRa;
-    private String tongDanhGia;
-    private String thanh;
+    private String tongDanhGia;*/
 
-    @Override
-    public String toString() {
-        return "SanPham{" +
-                "maSp=" + maSp +
-                ", tenSp='" + tenSp + '\'' +
-                ", maNcc=" + maNcc +
-                ", maLoai=" + maLoai +
-                ", soLuongTon=" + soLuongTon +
-                ", anhBia='" + anhBia + '\'' +
-                ", giaBan='" + giaBan + '\'' +
-                ", baoHanh='" + baoHanh + '\'' +
-                ", moTa='" + moTa + '\'' +
-                ", ngayCapNhat='" + ngayCapNhat + '\'' +
-                ", slBanRa=" + slBanRa +
-                ", tongDanhGia='" + tongDanhGia + '\'' +
-                '}';
+    public SanPham() {
     }
 
-    public SanPham(int maSp, String tenSp, int maNcc, int maLoai, int soLuongTon, String anhBia, String giaBan, String baoHanh, String moTa, String ngayCapNhat, int slBanRa, String tongDanhGia) {
+    public SanPham(int maSp, String tenSp) {
         this.maSp = maSp;
         this.tenSp = tenSp;
-        this.maNcc = maNcc;
-        this.maLoai = maLoai;
-        this.soLuongTon = soLuongTon;
-        this.anhBia = anhBia;
-        this.giaBan = giaBan;
-        this.baoHanh = baoHanh;
-        this.moTa = moTa;
-        this.ngayCapNhat = ngayCapNhat;
-        this.slBanRa = slBanRa;
-        this.tongDanhGia = tongDanhGia;
     }
 
     public int getMaSp() {
@@ -64,83 +38,11 @@ public class SanPham {
         this.tenSp = tenSp;
     }
 
-    public int getMaNcc() {
-        return maNcc;
-    }
-
-    public void setMaNcc(int maNcc) {
-        this.maNcc = maNcc;
-    }
-
-    public int getMaLoai() {
-        return maLoai;
-    }
-
-    public void setMaLoai(int maLoai) {
-        this.maLoai = maLoai;
-    }
-
-    public int getSoLuongTon() {
-        return soLuongTon;
-    }
-
-    public void setSoLuongTon(int soLuongTon) {
-        this.soLuongTon = soLuongTon;
-    }
-
-    public String getAnhBia() {
-        return anhBia;
-    }
-
-    public void setAnhBia(String anhBia) {
-        this.anhBia = anhBia;
-    }
-
-    public String getGiaBan() {
-        return giaBan;
-    }
-
-    public void setGiaBan(String giaBan) {
-        this.giaBan = giaBan;
-    }
-
-    public String getBaoHanh() {
-        return baoHanh;
-    }
-
-    public void setBaoHanh(String baoHanh) {
-        this.baoHanh = baoHanh;
-    }
-
-    public String getMoTa() {
-        return moTa;
-    }
-
-    public void setMoTa(String moTa) {
-        this.moTa = moTa;
-    }
-
-    public String getNgayCapNhat() {
-        return ngayCapNhat;
-    }
-
-    public void setNgayCapNhat(String ngayCapNhat) {
-        this.ngayCapNhat = ngayCapNhat;
-    }
-
-    public int getSlBanRa() {
-        return slBanRa;
-    }
-
-    public void setSlBanRa(int slBanRa) {
-        this.slBanRa = slBanRa;
-    }
-
-    public String getTongDanhGia() {
-        return tongDanhGia;
-    }
-
-    public void setTongDanhGia(String tongDanhGia) {
-        this.tongDanhGia = tongDanhGia;
+    @Override
+    public String toString() {
+        return "SanPham{" +
+                "maSp=" + maSp +
+                ", tenSp='" + tenSp + '\'' +
+                '}';
     }
 }
