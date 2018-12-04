81e8ff1bb9b08233bfbe5daf0c2da4caf48f7f73
= <=>
2018-12-04 15:07:51 +0700

    Thêm listview và đối tượng Sản phẩm

--- app/src/main/java/com/example/thanh/apphtlshop/MainActivity.java
+++ app/src/main/java/com/example/thanh/apphtlshop/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.thanh.apphtlshop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.example.thanh.model.SanPham;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView lvSanPham;
+    private ArrayAdapter<SanPham> arrSanPham;
+    private ArrayList<SanPham> listSanPham;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addControls();
+    }
+
+    private void addControls() {
+        lvSanPham = findViewById(R.id.lvSanPham);
+
     }
 }
